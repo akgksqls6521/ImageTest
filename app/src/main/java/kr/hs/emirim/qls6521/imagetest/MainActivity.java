4c7b7682023a27dccab9300713c880c3d61077a9
Hanbin Ma <qls6521@naver.com>
2016-09-27 20:21:42 +0900

    #4 event programming using onClick in Layout

--- app/src/main/java/kr/hs/emirim/qls6521/imagetest/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/qls6521/imagetest/MainActivity.java
@@ -7,15 +7,48 @@ import android.graphics.Canvas;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-public class MainActivity extends AppCompatActivity {
+import android.widget.LinearLayout;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity{
+    Myimage mImg;
+    static final int ORIGINAL=0;
+    static final int ROTATE=1;
+    static final int TRANSLATE=2;
+    static final int SCALE=3;
+    static final int SKEW=4;
+    int choose=ROTATE;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)  {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LinearLayout linear1=(LinearLayout)findViewById(R.id.linear1);
+        mImg=new Myimage(getApplicationContext());
+        linear1.addView(mImg);
+    }
+
+    public void transformImage(View v) {
+        switch(v.getId()){
+            case R.id.but_rotate:
+                choose=ROTATE;
+                break;
+            case R.id.but_translate:
+                choose=TRANSLATE;
+                break;
+            case R.id.but_scale:
+                choose=SCALE;
+                break;
+            case R.id.but_skew:
+                choose=SKEW;
+                break;
+        }
+        mImg.setChoose(choose);
+        mImg.invalidate();//onDraw가 다시호출됨. 다시 그리는 방법임!! choose값에 따라 원하는 도형을 그려준다
     }
 }
-/*    class MyImage extends View {
+   /*class MyImage extends View {
         MyImage(Context context) {
             super(context);//view를 상속받으면 무조건 이 생성자구문이 필요함! default생성자가 없어
         }
@@ -28,11 +61,11 @@ public class MainActivity extends AppCompatActivity {
             float cy=getWidth()/2.0f;
             float x=(getWidth()-picture.getWidth())/2.0f;
             float y=(getHeight()-picture.getHeight())/2.0f;
-            //canvas.rotate(45,cx,cy);
-            //canvas.translate(-150,200);
+            canvas.rotate(45,cx,cy);
+            canvas.translate(-150,200);
             canvas.scale(0.5f,0.5f,cx,cy);
-            //canvas.skew(0.4f,0.4f);
+            canvas.skew(0.4f,0.4f);
             canvas.drawBitmap(picture,x,y,null);
         }
-    }*/
-
+    }
+*/
--- app/src/main/java/kr/hs/emirim/qls6521/imagetest/Myimage.java
+++ app/src/main/java/kr/hs/emirim/qls6521/imagetest/Myimage.java
@@ -10,23 +10,41 @@ import android.view.View;
  * Created by appcreator12 on 2016. 9. 26..
  */
 public class Myimage extends View{
+    int choose=MainActivity.ORIGINAL;
+    Bitmap picture;
+
     Myimage(Context context) {
-        super(context);//view를 상속받으면 무조건 이 생성자구문이 필요함! default생성자가 없어
+        super(context);
+        picture= BitmapFactory.decodeResource(getResources(),R.drawable.iwa);//현재 프로젝트의 리소스불러오기
+
+    }
+    public void setChoose(int choose){
+        this.choose=choose;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {//view클래스의 onDraw를 오버라이딩하는 것이다
         super.onDraw(canvas);
-        Bitmap picture= BitmapFactory.decodeResource(getResources(),R.drawable.iwa);//현재 프로젝트의 리소스불러오기
         float cx=getWidth()/2.0f;
         float cy=getWidth()/2.0f;
         float x=(getWidth()-picture.getWidth())/2.0f;
         float y=(getHeight()-picture.getHeight())/2.0f;
-        //canvas.rotate(45,cx,cy);
-        //canvas.translate(-150,200);
-        canvas.scale(0.5f,0.5f,cx,cy);
-        //canvas.skew(0.4f,0.4f);
-        canvas.drawBitmap(picture,x,y,null);
+        switch(choose) {
+            case MainActivity.ROTATE:
+                canvas.rotate(45, cx, cy);
+                break;
+            case MainActivity.TRANSLATE:
+                canvas.translate(-150, 200);
+                break;
+            case MainActivity.SCALE:
+                canvas.scale(0.5f, 0.5f, cx, cy);
+                break;
+            case MainActivity.SKEW:
+                canvas.skew(0.4f, 0.4f);
+                break;
+        }
+        canvas.drawBitmap(picture, x, y, null);
+        //picture.recycle();
     }
 
 }
